04f26fe2597ae83b828dde002b945955c3572bdd
Dmitry Yakushin <ditros017@gmail.com>
2016-10-18 22:45:38 +0700

    Initial commit.

---
+++ FootballStats.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FootballStats.Web.MvcApplication" Language="C#" %>
---
+++ FootballStats.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FootballStats.Web.Startup))]
+namespace FootballStats.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
